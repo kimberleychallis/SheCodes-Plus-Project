3d9b5c70bfbe50f315eb8e963596594282a5eb51
Kimberley Challis <kimchallis@gmail.com>
2021-02-07 19:25:59 +0000

    Current weather API functionality (including geolocation)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-// ⛅️ Open Weather Map API Details
-
-const apiKey = "3e11ec91583e0c90e17fc5eef84e88aa";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?";
-
-// 📆 Display date and time
+// 📆 DISPLAY USER'S CURRENT DATE AND TIME
 
 const now = new Date();
 
@@ -44,22 +39,63 @@ const minutes = now.getMinutes();
 const dateTime = document.querySelector("#date-time");
 dateTime.innerHTML = `${day}, ${date} ${month} ${year} ${hour}:${minutes}`;
 
-// 🏙 Display city name
+// 🌞 OPEN WEATHER API
+
+//// ⛅️ Open Weather Map API Details
+
+const apiKey = "3e11ec91583e0c90e17fc5eef84e88aa";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?";
+
+//// 🏙 API call using user input
 
 function searchCity(event) {
   event.preventDefault();
 
   const cityInput = document.querySelector("#city-search-input");
   const city = cityInput.value;
+  const apiSearchString = `${apiURL}q=${city}&appid=${apiKey}&units=metric`;
 
-  const cityDisplay = document.querySelector("#city");
-  cityDisplay.innerHTML = `${city}`;
+  axios.get(apiSearchString).then(displayWeather);
+}
+
+//// 🗺 Obtain current location and API call using geolocation
+
+function geolocateUser(event) {
+  event.preventDefault();
+
+  navigator.geolocation.getCurrentPosition(obtainWeather);
+}
+
+function obtainWeather(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const coords = `lat=${lat}&lon=${lon}`;
+  const apiSearchString = `${apiURL}${coords}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiSearchString).then(displayWeather);
+}
+
+const geolocateButton = document.querySelector("#geolocate");
+geolocateButton.addEventListener("click", geolocateUser);
+
+//// 🏙 Display city name and current temperature (input or geolocation)
+
+function displayWeather(response) {
+  const temperature = Math.round(response.data.main.temp);
+  const cityName = response.data.name;
+
+  const cityNameDisplay = document.querySelector("#city");
+  cityNameDisplay.innerHTML = `${cityName}`;
+
+  const nowTemperature = document.querySelector("#current-temperature");
+  nowTemperature.innerHTML = `${temperature}°`;
 }
 
 const search = document.querySelector("form");
 search.addEventListener("submit", searchCity);
 
-// 🌡 Switch between celsius and fahrenheit
+// 🌡 SWITCH BETWEEN CELSIUS AND FAHRENHEIT
+// Will probably need to split these out into functions that do the conversion
 
 function convertTemp() {
   const switchButton = document.querySelector("#switch");
@@ -67,9 +103,29 @@ function convertTemp() {
 
   const celsius = buttonContent.includes("celsius");
   if (celsius === true) {
+    // toggles 'button'
     switchButton.innerHTML = `<i class="fas fa-sync-alt"></i> switch to farenheit`;
+
+    // switches temperatures to celsius
+
+    const tempFahrenheitString = document.querySelector("#current-temperature");
+    const tempFahrenheit = parseInt(tempFahrenheitString.innerText);
+    const tempCelsius = Math.round(((tempFahrenheit - 32) * 5) / 9);
+
+    const currentTemperature = document.querySelector("#current-temperature");
+    currentTemperature.innerHTML = `${tempCelsius}`;
   } else {
+    // toggles 'button'
     switchButton.innerHTML = `<i class="fas fa-sync-alt"></i> switch to celsius`;
+
+    // switches temperatures to fahrenheit
+
+    const tempCelsiusString = document.querySelector("#current-temperature");
+    const tempCelsius = parseInt(tempCelsiusString.innerText);
+    const tempFahrenheit = Math.round((tempCelsius * 9) / 5 + 32);
+
+    const currentTemperature = document.querySelector("#current-temperature");
+    currentTemperature.innerHTML = `${tempFahrenheit}`;
   }
 }
 
